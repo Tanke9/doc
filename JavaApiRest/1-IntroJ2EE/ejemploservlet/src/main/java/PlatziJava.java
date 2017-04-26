6f67867570e1b86739363b3d253272025459a6e9
Alberto Ruiz <albertocantabria@gmail.com>
2017-04-26 16:46:37 +0200

    JavaServlet

---
+++ JavaApiRest/1-IntroJ2EE/ejemploservlet/src/main/java/PlatziJava.java
@@ -0,0 +1,31 @@
+package platzi.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/platzi-java")
+public class PlatziJava extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String message = req.getParameter("message");
+		try (PrintWriter out = resp.getWriter()){
+			out.print("{\"message\": \"" + message + "\"}");
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+}
